d3f86bb0bd7833eaf7eff38be970f2a29a85338e
Christopher Wan <christopher.wan@yale.edu>
2015-08-20 20:47:39 -0400

    downloading and editing from parse

--- ParseStarterProject/ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ParseStarterProject/ViewController.swift
@@ -13,17 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success, error) -> Void in
-            
-            if (success) {
-                print("Object has been saved.")
-            } else {
-                print("Failed.")
-            }
-        }
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "baz"
+//        testObject.saveInBackgroundWithBlock { (success, error) -> Void in
+//            
+//            if (success) {
+//                print("Object has been saved.")
+//            } else {
+//                print("Failed.")
+//            }
+//        }
         
+        var query = PFQuery(className: "TestObject")
+        query.getObjectInBackgroundWithId("Z7WyWhzeyc", block: {(object: PFObject?, error: NSError?) -> Void in
+            if(error != nil) {
+                print(error)
+            } else if let test = object{
+                test["foo"] = "bar"
+                test.saveInBackground()
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
