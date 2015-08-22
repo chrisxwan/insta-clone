4994dc180fc48c2895139d2231bdd9f18900e1d4
Christopher Wan <christopher.wan@yale.edu>
2015-08-21 21:32:09 -0400

    start working on displaying user list

--- ParseStarterProject/ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ParseStarterProject/ViewController.swift
@@ -48,7 +48,7 @@ class ViewController: UIViewController {
                     self.activityIndicator.stopAnimating()
                     UIApplication.sharedApplication().endIgnoringInteractionEvents()
                     if(error == nil) {
-                        
+                        self.performSegueWithIdentifier("login", sender: self)
                     } else {
                         if let errorString = error!.userInfo["error"] as? String {
                             errorMessage = errorString
@@ -62,6 +62,7 @@ class ViewController: UIViewController {
                     self.activityIndicator.stopAnimating()
                     UIApplication.sharedApplication().endIgnoringInteractionEvents()
                     if(user != nil) {
+                        self.performSegueWithIdentifier("login", sender: self)
                     } else {
                         if let errorString = error!.userInfo["error"] as? String {
                             errorMessage = errorString
@@ -91,6 +92,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if(PFUser.currentUser() != nil) {
+            self.performSegueWithIdentifier("login", sender: self)
+        }
     }
 
     override func didReceiveMemoryWarning() {
