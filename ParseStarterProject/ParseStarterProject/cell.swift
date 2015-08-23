b2c005fb388a56bdedd9c69da172e65ecd2409cb
Christopher Wan <christopher.wan@yale.edu>
2015-08-22 23:43:16 -0400

    pretty much done

---
+++ ParseStarterProject/ParseStarterProject/cell.swift
@@ -0,0 +1,18 @@
+//
+//  cell.swift
+//  ParseStarterProject
+//
+//  Created by Christopher Wan on 8/22/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+    @IBOutlet var postedImage: UIImageView!
+    @IBOutlet var username: UILabel!
+    @IBOutlet var message: UILabel!
+    
+    
+}
